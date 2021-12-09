b8074465b2dc3e41ebb7346e1bf907a374419818
WarunOhm <warunyoo__hiphop@hotmail.com>
2021-12-09 21:20:01 +0700

    ohm

--- application/controllers/Dental.php
+++ application/controllers/Dental.php
@@ -32,8 +32,7 @@ class Dental extends CI_Controller
         $sql = "select o.oqueue,o.vstdate,p.cid,o.hn,o.vsttime,o.vn,concat(p.pname,p.fname,' ',p.lname) as ptname,v.age_y,v.age_m,v.age_d,
         k.depcode as curdep_code,k.department as curdep_name from ovst o left outer join vn_stat v   on v.vn = o.vn left outer join patient p  on p.hn = o.hn
         left outer join kskdepartment k on k.depcode = o.cur_dep
-                where k.spclty = '11' AND
-                 o.vstdate >= CURDATE() ORDER BY o.oqueue ASC Limit 4 ;";
+                where k.spclty = '11'  ORDER BY o.oqueue desc Limit 4 ;";
         $query = $this->db->query($sql);
         foreach ($query->result() as $row) {
             $data = array(
