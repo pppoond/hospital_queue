3fc07197eb1d21f5c10cebca12e15dcfbbf186e1
OhmWarun <warunyoo__hiphop@hotmail.com>
2021-12-16 15:16:17 +0700

    com

--- application/controllers/Speech.php
+++ application/controllers/Speech.php
@@ -8,11 +8,9 @@ class Speech extends CI_Controller
 
 	public function index()
 	{
-
 		$this->load->helper('url');
 		$this->load->library('javascript');
 		$this->load->view('includes/css');
-
 		$this->load->view('speech_view');
 	}
 }
--- application/views/pages/dental/dental_qmain.php
+++ application/views/pages/dental/dental_qmain.php
@@ -1,22 +1,4 @@
-<?php
-$url = 'http://localhost/hospital_queue/api_speech/speechapi';;
-$content = file_get_contents($url);
-$json = json_decode($content, true);
 
-foreach ($json as $item) {
-?>
-<div class="row">
-    <textarea>   <?php echo "เชิญหมายเลข{$item['oqueue']}ที่ห้อง{$item['curdep_name']}ครับ"; ?>   </textarea>
-    </div>
-
-    <div class="row ">
-        <button>Convert To Speech</button>
-    </div>
-    </form>
-    </div>
-
-
-<?php } ?>
 
 <?php
 if ($num_row == 1) {
@@ -32,11 +14,6 @@ if ($num_row == 1) {
         <div class="card3 g-color" style="background-color: #616EFF;color: white;">
             <h3 style="padding: 0;margin: 0;font-weight: 600;"><?php echo date("H:i", strtotime("$sign_datetime")); ?></h6>
                 <!-- <textarea  name="hide" style="display:none; id="" cols="30" rows="10"><?php echo $oqueue; ?></textarea> -->
-
-                <textarea name="" id="" cols="30" rows="10"><?php echo $oqueue; ?></textarea>
-
-
-                <textarea id="" cols="30" rows="10"><?php echo "เชิญหมายเลข{$item['oqueue']}ที่ห้อง{$item['curdep_name']}ครับ"; ?></textarea>
 
 
         </div>
